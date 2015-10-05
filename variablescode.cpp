45c794c4ca953f5c8c97a8adf0fd8e1cf699baa6
rudman <Shaina.Rudman@gmail.com>
2015-10-05 04:38:39 -0400

    test command

---
+++ variablescode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  cout << "hellow world" << end;
+  int i=2
+    cout << "i = " <<i<<endl;
+ 
+  double a=3.3;
+  cout << "a = " <<a<<endl;
+ 
+  int k =a*i;
+ 
+  cout << "a*i = "<<j<<endl;
+ 
+  return 0;
+]
+# HONRPHYS
