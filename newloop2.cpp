9bcb4a051c4bf7b767342c1f91d8ddc6f428aab6
rudman <Shaina.Rudman@gmail.com>
2015-10-06 01:50:03 -0400

    test command

---
+++ newloop2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main(){
+
+  for (int n=0; n<10; n++) {
+    cout << "n is " << n << ": ";
+    for (int m=0; m<=n; m++) {
+      cout << m;
+   }
+  cout << endl;
+  }
+  return 0;
+}
+# HONRPHYS
