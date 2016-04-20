59cc50f5f67f0ddb8e43dcfc3c10f2724607fb17
Shaina Rudman <Shaina.Rudman@gmail.com>
2016-04-19 23:12:22 -0400

    Delete main.cpp

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-  cout <<"Hello World!" << endl;   //Print hello wirkd to screen followed by end line (endl)
-  return 0;   //Exit the program 
-}
